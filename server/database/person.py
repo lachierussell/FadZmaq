fe9ca06899c00030584ca34042411f90ae1e3336
Lachlan Russell <lachie.russell@gmail.com>
2019-08-29 11:34:06 +0800

    Updated file comments

--- server/database/person.py
+++ server/database/person.py
@@ -1,3 +1,14 @@
+# @file
+# @brief        TEMPORARY TEST FILE FOR ORM MODELS
+# fadzmaq.py
+#
+# FadZmaq Project
+# Professional Computing. Semester 2 2019
+#
+#
+# Copyright FadZmaq © 2019      All rights reserved.
+# @author Lachlan Russell       22414249@student.uwa.edu.au
+
 from sqlalchemy import Column, String, Integer, Date
 from . import Base
 
