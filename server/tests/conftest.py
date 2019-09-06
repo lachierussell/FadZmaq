3b2422369fc240f894a10940c84f2c7ac6de27df
jordanvr <jordash@live.com>
2019-09-06 14:04:47 +0800

    Tests to meet refactoring

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -3,12 +3,18 @@ import pytest
 from fadzmaq import create_app
 from fadzmaq import database
 
+from fadzmaq.database.db import get_db, get_engine
+
 @pytest.fixture
 def api():
 
     api = create_app({
         'TESTING': True,
     })
+
+    with api.app_context():
+        get_engine()
+        get_db()
 
     yield api
 
@@ -18,10 +24,11 @@ def client(api):
     """A test client for the app."""
     return api.test_client()
 
-@pytest.fixture
-def db():
-    """A test connection to the database"""
-    return database
+# @pytest.fixture
+# def db():
+#     """A test connection to the database"""
+#     return g.db
+#     # return database
 
 
 
--- server/tests/test_startup.py
+++ server/tests/test_startup.py
@@ -1,4 +1,6 @@
 import pytest
+from fadzmaq.database.db import get_db, get_engine
+import sqlalchemy
 
 # Tests that the server is up at all.
 # Code is 300 because we shouldn't be using index (I think?) - Jordan 
@@ -7,16 +9,26 @@ def test_index(client):
     assert response.status_code == 300
 
 
-def test_database(db):
-    results = db.engine.execute('SELECT 1')
-    print(results.first())
+# def test_database(client):
+#     results = db.engine.execute('SELECT 1')
+#     print(results.first())
 
+def test_get_close_db(api):
+    with api.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sqlalchemy.exc.StatementError) as e:
+        db.execute('SELECT 1')
+
+    assert 'closed' in str(e.value)
 
 # Tests the correct tables are present
-def test_tables(db):
-
-    assert db.engine.dialect.has_table(db.engine, "primary_user")
-    assert db.engine.dialect.has_table(db.engine, "hobbies")
-    assert db.engine.dialect.has_table(db.engine, "matches")
-    assert db.engine.dialect.has_table(db.engine, "votes")
+def test_tables(api):
+    with api.app_context():
+        engine = get_engine()
+        assert engine.dialect.has_table(engine, "primary_user")
+        assert engine.dialect.has_table(engine, "hobbies")
+        assert engine.dialect.has_table(engine, "matches")
+        assert engine.dialect.has_table(engine, "votes")
   
