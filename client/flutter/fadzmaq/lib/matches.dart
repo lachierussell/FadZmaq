4e7832cf5b05a4328c8158ec87e5fe96a883340a
jordanvr <jordash@live.com>
2019-09-10 22:58:27 +0800

    Added test people

--- client/flutter/fadzmaq/lib/matches.dart
+++ client/flutter/fadzmaq/lib/matches.dart
@@ -31,14 +31,15 @@ class MatchesPage extends StatelessWidget {
               borderRadius: BorderRadius.all(Radius.circular(10)),
               child: Image.network(
                 profile.imageURL,
-                height: 96,
-                width: 96,
+                height: 80,
+                width: 80,
                 fit: BoxFit.cover,
               ),
             ),
           ),
           Expanded(
             child: Container(
+              // color: Colors.green,
               padding: const EdgeInsets.only(left: 16),
               // alignment: Alignment.centerLeft,
               child: getMatchText(context, profile),
@@ -55,7 +56,8 @@ class MatchesPage extends StatelessWidget {
       children: <Widget>[
         Text(
           profile.name,
-          style: Theme.of(context).textTheme.title,
+          // style: Theme.of(context).textTheme.title,
+          style: nameStyle,
         ),
         getHobbies(context, profile),
       ],
@@ -88,7 +90,8 @@ class MatchesPage extends StatelessWidget {
         color: hobby.color,
         child: Padding(
           padding: const EdgeInsets.fromLTRB(10, 4, 10, 4),
-          child: new Text(hobby.name, style: Theme.of(context).textTheme.body1),
+          // child: new Text(hobby.name, style: Theme.of(context).textTheme.body1),
+          child: new Text(hobby.name, style: hobbyStyle)
         ),
       ),
     );
@@ -109,6 +112,16 @@ class MatchesPage extends StatelessWidget {
   }
 }
 
+final TextStyle nameStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+final TextStyle hobbyStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+);
+
 class MatchedProfile {
   const MatchedProfile({this.name, this.imageURL, this.hobbies});
   final String name;
@@ -123,8 +136,27 @@ class Hobby {
 }
 
 List<MatchedProfile> _matchedProfiles = <MatchedProfile>[
-  _davidTennant,
-  _seanBean
+  _david,
+  _sean,
+  _jeffrey,
+  _rachel,
+  _sam,
+  _ryan,
+  _rooney,
+  _david,
+  _sean,
+  _jeffrey,
+  _rachel,
+  _sam,
+  _ryan,
+  _rooney,
+  _david,
+  _sean,
+  _jeffrey,
+  _rachel,
+  _sam,
+  _ryan,
+  _rooney,
 ];
 
 final List<Hobby> _hobbies = <Hobby>[
@@ -149,14 +181,44 @@ List<Hobby> getRandomHobbies(int n) {
   return list;
 }
 
-MatchedProfile _seanBean = MatchedProfile(
+MatchedProfile _sean = MatchedProfile(
     name: "Sean Bean",
     imageURL:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/AV0A6306_Sean_Bean.jpg/468px-AV0A6306_Sean_Bean.jpg",
     hobbies: getRandomHobbies(5));
 
-MatchedProfile _davidTennant = MatchedProfile(
+MatchedProfile _david = MatchedProfile(
     name: "David Tennant",
     imageURL:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Good_Omens_panel_at_NYCC_%2860841%29a.jpg/1024px-Good_Omens_panel_at_NYCC_%2860841%29a.jpg",
+    hobbies: getRandomHobbies(2));
+
+MatchedProfile _rooney = MatchedProfile(
+    name: "Rooney Mara",
+    imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/1/10/Rooney_Mara_at_The_Discovery_premiere_during_day_2_of_the_2017_Sundance_Film_Festival_at_Eccles_Center_Theatre_on_January_20%2C_2017_in_Park_City%2C_Utah_%2832088061480%29_%28cropped%29.jpg",
+    hobbies: getRandomHobbies(2));
+
+MatchedProfile _jeffrey = MatchedProfile(
+    name: "Jeffrey Wright",
+    imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+    hobbies: getRandomHobbies(2));
+
+MatchedProfile _sam = MatchedProfile(
+    name: "Sam Neill",
+    imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sam_Neill_2010.jpg/435px-Sam_Neill_2010.jpg",
+    hobbies: getRandomHobbies(2));
+
+MatchedProfile _ryan = MatchedProfile(
+    name: "Ryan Gosling",
+    imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Ryan_Gosling_%2836034827222%29_%28cropped%29.jpg/428px-Ryan_Gosling_%2836034827222%29_%28cropped%29.jpg",
+    hobbies: getRandomHobbies(2));
+
+MatchedProfile _rachel = MatchedProfile(
+    name: "Rachel McAdams",
+    imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/3/3e/Rachel_McAdams%2C_2016_%28cropped%29.jpg",
     hobbies: getRandomHobbies(2));
