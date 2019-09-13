ab4b3ad13ec161e081cd17b0d8f80cb87edfcd9d
jordanvr <jordash@live.com>
2019-09-13 16:24:31 +0800

    random hobbies and quoted out static data

--- client/flutter/fadzmaq/lib/views/matches.dart
+++ client/flutter/fadzmaq/lib/views/matches.dart
@@ -1,3 +1,4 @@
+import 'package:fadzmaq/views/profilepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:fadzmaq/controllers/request.dart';
@@ -18,8 +19,6 @@ class MatchesTempApp extends StatelessWidget {
 class MatchesPage extends StatelessWidget {
   const MatchesPage([Key key]) : super(key: key);
 
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,11 +36,8 @@ class MatchesPage extends StatelessWidget {
 }
 
 class MatchesList extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-
     MatchesData matchesData = RequestProvider.of<MatchesData>(context);
     print(matchesData.toString());
     print(matchesData.matches.toString());
@@ -59,31 +55,40 @@ class MatchesList extends StatelessWidget {
   }
 
   Widget _listItem(BuildContext context, MatchProfileData profile) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            child: ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              child: Image.network(
-                profile.photo,
-                height: 80,
-                width: 80,
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ProfilePage()),
+        );
+      },
+      behavior: HitTestBehavior.opaque,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: Image.network(
+                  profile.photo,
+                  height: 80,
+                  width: 80,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              // color: Colors.green,
-              padding: const EdgeInsets.only(left: 16),
-              // alignment: Alignment.centerLeft,
-              child: getMatchText(context, profile),
+            Expanded(
+              child: Container(
+                // color: Colors.green,
+                padding: const EdgeInsets.only(left: 16),
+                // alignment: Alignment.centerLeft,
+                child: getMatchText(context, profile),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -103,21 +108,22 @@ class MatchesList extends StatelessWidget {
   }
 
   Widget getHobbies(BuildContext context, MatchProfileData profile) {
+    // return Text("to be done");
 
-    return Text("to be done");
-
-    // List<Widget> list = new List<Widget>();
+    List<Widget> list = new List<Widget>();
+    List<Hobby> hobsTemp = getRandomHobbies(2);
     // for (Hobby hobby in profile.hobbies) {
-    //   list.add(getHobbyChip(context, hobby));
-    // }
-    // return Padding(
-    //   padding: const EdgeInsets.only(top: 8),
-    //   child: new Wrap(
-    //     spacing: 4,
-    //     runSpacing: 4,
-    //     children: list,
-    //   ),
-    // );
+    for (Hobby hobby in hobsTemp) {
+      list.add(getHobbyChip(context, hobby));
+    }
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: new Wrap(
+        spacing: 4,
+        runSpacing: 4,
+        children: list,
+      ),
+    );
   }
 
   Widget getHobbyChip(BuildContext context, Hobby hobby) {
@@ -148,12 +154,12 @@ final TextStyle hobbyStyle = TextStyle(
   fontWeight: FontWeight.w400,
 );
 
-class MatchedProfile {
-  const MatchedProfile({this.name, this.imageURL, this.hobbies});
-  final String name;
-  final String imageURL;
-  final List<Hobby> hobbies;
-}
+// class MatchedProfile {
+//   const MatchedProfile({this.name, this.imageURL, this.hobbies});
+//   final String name;
+//   final String imageURL;
+//   final List<Hobby> hobbies;
+// }
 
 class Hobby {
   const Hobby({this.name, this.color});
@@ -161,29 +167,29 @@ class Hobby {
   final Color color;
 }
 
-List<MatchedProfile> _matchedProfiles = <MatchedProfile>[
-  _david,
-  _sean,
-  _jeffrey,
-  _rachel,
-  _sam,
-  _ryan,
-  _rooney,
-  _david,
-  _sean,
-  _jeffrey,
-  _rachel,
-  _sam,
-  _ryan,
-  _rooney,
-  _david,
-  _sean,
-  _jeffrey,
-  _rachel,
-  _sam,
-  _ryan,
-  _rooney,
-];
+// List<MatchedProfile> _matchedProfiles = <MatchedProfile>[
+//   _david,
+//   _sean,
+//   _jeffrey,
+//   _rachel,
+//   _sam,
+//   _ryan,
+//   _rooney,
+//   _david,
+//   _sean,
+//   _jeffrey,
+//   _rachel,
+//   _sam,
+//   _ryan,
+//   _rooney,
+//   _david,
+//   _sean,
+//   _jeffrey,
+//   _rachel,
+//   _sam,
+//   _ryan,
+//   _rooney,
+// ];
 
 final List<Hobby> _hobbies = <Hobby>[
   Hobby(name: "Hiking", color: Color(0xfffbb4ae)),
@@ -207,44 +213,44 @@ List<Hobby> getRandomHobbies(int n) {
   return list;
 }
 
-MatchedProfile _sean = MatchedProfile(
-    name: "Sean Bean",
-    imageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/AV0A6306_Sean_Bean.jpg/468px-AV0A6306_Sean_Bean.jpg",
-    hobbies: getRandomHobbies(5));
+// MatchedProfile _sean = MatchedProfile(
+//     name: "Sean Bean",
+//     imageURL:
+//         "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/AV0A6306_Sean_Bean.jpg/468px-AV0A6306_Sean_Bean.jpg",
+//     hobbies: getRandomHobbies(5));
 
-MatchedProfile _david = MatchedProfile(
-    name: "David Tennant",
-    imageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Good_Omens_panel_at_NYCC_%2860841%29a.jpg/1024px-Good_Omens_panel_at_NYCC_%2860841%29a.jpg",
-    hobbies: getRandomHobbies(2));
+// MatchedProfile _david = MatchedProfile(
+//     name: "David Tennant",
+//     imageURL:
+//         "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Good_Omens_panel_at_NYCC_%2860841%29a.jpg/1024px-Good_Omens_panel_at_NYCC_%2860841%29a.jpg",
+//     hobbies: getRandomHobbies(2));
 
-MatchedProfile _rooney = MatchedProfile(
-    name: "Rooney Mara",
-    imageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/1/10/Rooney_Mara_at_The_Discovery_premiere_during_day_2_of_the_2017_Sundance_Film_Festival_at_Eccles_Center_Theatre_on_January_20%2C_2017_in_Park_City%2C_Utah_%2832088061480%29_%28cropped%29.jpg",
-    hobbies: getRandomHobbies(2));
+// MatchedProfile _rooney = MatchedProfile(
+//     name: "Rooney Mara",
+//     imageURL:
+//         "https://upload.wikimedia.org/wikipedia/commons/1/10/Rooney_Mara_at_The_Discovery_premiere_during_day_2_of_the_2017_Sundance_Film_Festival_at_Eccles_Center_Theatre_on_January_20%2C_2017_in_Park_City%2C_Utah_%2832088061480%29_%28cropped%29.jpg",
+//     hobbies: getRandomHobbies(2));
 
-MatchedProfile _jeffrey = MatchedProfile(
-    name: "Jeffrey Wright",
-    imageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
-    hobbies: getRandomHobbies(2));
+// MatchedProfile _jeffrey = MatchedProfile(
+//     name: "Jeffrey Wright",
+//     imageURL:
+//         "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+//     hobbies: getRandomHobbies(2));
 
-MatchedProfile _sam = MatchedProfile(
-    name: "Sam Neill",
-    imageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sam_Neill_2010.jpg/435px-Sam_Neill_2010.jpg",
-    hobbies: getRandomHobbies(2));
+// MatchedProfile _sam = MatchedProfile(
+//     name: "Sam Neill",
+//     imageURL:
+//         "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sam_Neill_2010.jpg/435px-Sam_Neill_2010.jpg",
+//     hobbies: getRandomHobbies(2));
 
-MatchedProfile _ryan = MatchedProfile(
-    name: "Ryan Gosling",
-    imageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Ryan_Gosling_%2836034827222%29_%28cropped%29.jpg/428px-Ryan_Gosling_%2836034827222%29_%28cropped%29.jpg",
-    hobbies: getRandomHobbies(2));
+// MatchedProfile _ryan = MatchedProfile(
+//     name: "Ryan Gosling",
+//     imageURL:
+//         "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Ryan_Gosling_%2836034827222%29_%28cropped%29.jpg/428px-Ryan_Gosling_%2836034827222%29_%28cropped%29.jpg",
+//     hobbies: getRandomHobbies(2));
 
-MatchedProfile _rachel = MatchedProfile(
-    name: "Rachel McAdams",
-    imageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/3/3e/Rachel_McAdams%2C_2016_%28cropped%29.jpg",
-    hobbies: getRandomHobbies(2));
+// MatchedProfile _rachel = MatchedProfile(
+//     name: "Rachel McAdams",
+//     imageURL:
+//         "https://upload.wikimedia.org/wikipedia/commons/3/3e/Rachel_McAdams%2C_2016_%28cropped%29.jpg",
+//     hobbies: getRandomHobbies(2));
