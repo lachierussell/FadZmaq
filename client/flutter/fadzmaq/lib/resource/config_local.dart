756392cb9387dbcb63e1dc231481fda6567f9677
Lachlan Russell <lachie.russell@gmail.com>
2019-09-13 18:40:00 +0800

    api test file

--- client/flutter/fadzmaq/lib/resource/config_local.dart
+++ client/flutter/fadzmaq/lib/resource/config_local.dart
@@ -3,6 +3,7 @@ import 'package:fadzmaq/models/app_config.dart';
 class ConfigResourceDev implements ConfigResource {
   @override
   String testString = "Test string devopment";
-   String server = "http://localhost:5000/";
+//   String server = "http://localhost:5000/";
 //    String server = "http://10.0.2.2:5000/";
+  String server = "http://fadzmaq.russell-systems.cc/";
 }
