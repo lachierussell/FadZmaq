707ec75ae6ef5e436937f31a63f7caa8820970c9
Thiren Naidoo <22257963@student.uwa.edu.au>
2019-09-14 22:00:59 +0800

    Added model for hobbies

---
+++ client/flutter/fadzmaq/lib/models/hobbies.dart
@@ -0,0 +1,33 @@
+class HobbyData {
+  List<HobbyData> hobbies;
+
+  HobbyData({this.hobbies});
+
+  factory HobbyData.fromJson(Map<String, dynamic> json) =>
+      _hobbiesFromJson(json);
+}
+
+HobbyData _hobbiesFromJson(Map<String, dynamic> json) {
+  var hobbiesJson = json['hobby_list'] as List;
+  List<HobbyData> hobbies = hobbiesJson != null
+      ? hobbiesJson.map((i) => HobbyData.fromJson(i)).toList()
+      : null;
+
+  return HobbyData(
+    hobbies : hobbies,
+  );
+}
+
+class HobbyListData {
+  final String id;
+  final String name;
+  
+  HobbyListData({this.id, this.name});
+
+  factory HobbyListData.fromJson(Map<String, dynamic> json) {
+    return HobbyListData(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+}
