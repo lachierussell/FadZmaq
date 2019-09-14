3539e1f2c59998dda80bb9bc3690d5609a835fa1
jordanvr <jordash@live.com>
2019-09-14 18:49:01 +0800

    added re,pte server cto config

--- client/flutter/fadzmaq/lib/resource/config_testserver.dart
+++ client/flutter/fadzmaq/lib/resource/config_testserver.dart
@@ -3,5 +3,5 @@ import 'package:fadzmaq/models/app_config.dart';
 class ConfigResourceTestServer implements ConfigResource {
   @override
   String testString = "Test string test server";
-  String server = "127.0.0.1";
+  String server = "http://fadzmaq.russell-systems.cc/";
 }
