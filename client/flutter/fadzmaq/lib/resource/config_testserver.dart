7f9ecbf998dc14ccf23ec06581148c9c6a51f6b8
Lachlan Russell <lachie.russell@gmail.com>
2019-09-15 13:50:03 +0800

    Https for server config

--- client/flutter/fadzmaq/lib/resource/config_testserver.dart
+++ client/flutter/fadzmaq/lib/resource/config_testserver.dart
@@ -3,5 +3,5 @@ import 'package:fadzmaq/models/app_config.dart';
 class ConfigResourceTestServer implements ConfigResource {
   @override
   String testString = "Test string test server";
-  String server = "http://fadzmaq.russell-systems.cc/";
+  String server = "https://fadzmaq.russell-systems.cc/";
 }
