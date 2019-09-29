a9b5cc0ae99bc07d27373f80a2c3caee88e723d6
Lachlan Russell <lachie.russell@gmail.com>
2019-09-29 14:03:23 +0800

    db trigger works now but has potential for duplicate match

--- server/fadzmaq/database/init.sql
+++ server/fadzmaq/database/init.sql
@@ -17,8 +17,9 @@ DROP TABLE IF EXISTS matches;
 DROP VIEW IF EXISTS matches_v;
 DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS hobbies;
+DROP TABLE IF EXISTS user_location;
+DROP TABLE IF EXISTS location_data;
 DROP TABLE IF EXISTS profile;
-DROP TABLE IF EXISTS primary_user;
 DROP TYPE IF EXISTS HOBBY_SWAP;
 
 -- TODO reinstate this in sprint 3
@@ -35,14 +36,13 @@ CREATE TABLE IF NOT EXISTS profile
     dob      TIMESTAMP    DEFAULT NULL,
     email    VARCHAR(255) UNIQUE NOT NULL,
     phone    VARCHAR      UNIQUE,
-    photo    VARCHAR      DEFAULT NULL
+    photo    VARCHAR      DEFAULT NULL,
+    distance INT          DEFAULT 20 NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS matches
 (
-    match_id SERIAL  NOT NULL
-        CONSTRAINT matches_pk
-            PRIMARY KEY,
+    match_id SERIAL  NOT NULL CONSTRAINT matches_pk PRIMARY KEY,
     user_a   VARCHAR NOT NULL REFERENCES profile (user_id),
     user_b   VARCHAR NOT NULL REFERENCES profile (user_id),
     time     TIME,
@@ -72,8 +72,61 @@ CREATE TABLE IF NOT EXISTS user_hobbies
     swap     HOBBY_SWAP NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS location_data
+(
+    location_id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+    lat         FLOAT NOT NULL,
+    long        FLOAT NOT NULL
+);
 
--- Inserting dummy values --
+CREATE TABLE IF NOT EXISTS user_location
+(
+    user_id         VARCHAR NOT NULL REFERENCES profile (user_id),
+    location_id     SERIAL NOT NULL REFERENCES location_data (location_id)
+);
+
+
+--------------------------------------------
+--  ----------------------------------------
+--  FUNCTIONS
+--  ----------------------------------------
+--------------------------------------------
+
+-- TODO reinstate this in sprint 3
+CREATE OR REPLACE FUNCTION match()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS
+$make_match$
+BEGIN
+    IF ( (
+            SELECT v.user_from
+            FROM votes v
+            WHERE NEW.vote
+              AND v.vote
+              AND v.user_to = new.user_from
+              AND new.user_to = v.user_from
+         ) NOTNULL
+    ) THEN
+        INSERT INTO matches (user_a, user_b, time, rating)
+          VALUES (NEW.user_from, NEW.user_to, now(), null);
+        DELETE FROM votes WHERE user_to = NEW.user_from;
+        RETURN NULL;
+    END IF;
+    RETURN NEW;
+END;
+$make_match$;
+
+CREATE TRIGGER make_match BEFORE INSERT OR UPDATE ON votes
+    FOR EACH ROW EXECUTE FUNCTION match();
+
+
+--------------------------------------------
+--  ----------------------------------------
+--  Dummy Data
+--  ----------------------------------------
+--------------------------------------------
+
 -- DOB IN USA (month/day/year)
 INSERT INTO profile (bio, nickname, email, dob, phone, user_id, photo)
 VALUES ('Avid rock climber and hiking enthusiast.', 'Lachie', 'Lachie@email.com', '1999-09-04', '0423199199', 'b026324c6904b2a9cb4b88d6d61c81d1',
@@ -121,10 +174,6 @@ VALUES ('HJtnPGdccnbqsR1V0hWSJe9AWFx1', 'Thiren', 'Thiren is the primary contact
                                                   'behalf of the team.', '1998-09-8', '22239906@student.uwa.edu.au',
         '0400100500', 'https://www.russell-systems.cc/other/ceac483c49a4b8c2c03e4eb3b7e213b8746b996bb7dd30468e0ea6044710a648.jpg');
 
--- INSERT INTO profile (user_id, nickname, bio, dob, email, phone, photo)
--- VALUES ('', 'Rupert', 'Add your bio here:', '02/16/1998', '22239906@student.uwa.edu.au',
---         '0400100600', 'https://www.russell-systems.cc/other/ceac483c49a4b8c2c03e4eb3b7e213b8746b996bb7dd30468e0ea6044710a648.jpg');
-
 
 INSERT INTO hobbies (name) VALUES ('Boxing');
 INSERT INTO hobbies (name) VALUES ('Boating');
@@ -141,30 +190,30 @@ INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('26ab0db90d72e28ad0ba
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('b026324c6904b2a9cb4b88d6d61c81d1', 4, 'discover');
 
 -- Test data for matches (John has three, a few others have one with him)
-INSERT INTO matches (user_a, user_b)
-VALUES ('26ab0db90d72e28ad0ba1e22ee510510', '6d7fce9fee471194aa8b5b6e47267f03');
-INSERT INTO matches (user_a, user_b)
-VALUES ('26ab0db90d72e28ad0ba1e22ee510510', 'b026324c6904b2a9cb4b88d6d61c81d1');
-INSERT INTO matches (user_a, user_b)
-VALUES ('26ab0db90d72e28ad0ba1e22ee510510', '48a24b70a0b376535542b996af517398');
+-- INSERT INTO matches (user_a, user_b)
+-- VALUES ('26ab0db90d72e28ad0ba1e22ee510510', '6d7fce9fee471194aa8b5b6e47267f03');
+-- INSERT INTO matches (user_a, user_b)
+-- VALUES ('26ab0db90d72e28ad0ba1e22ee510510', 'b026324c6904b2a9cb4b88d6d61c81d1');
+-- INSERT INTO matches (user_a, user_b)
+-- VALUES ('26ab0db90d72e28ad0ba1e22ee510510', '48a24b70a0b376535542b996af517398');
 
 --------------------------------------------
 --  ----------------------------------------
---  USER DATA FOR THE TEAM
+--  DUMMY USER DATA FOR THE TEAM
 --  ----------------------------------------
 --------------------------------------------
 
 -- Lachie
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', '26ab0db90d72e28ad0ba1e22ee510510', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 'b026324c6904b2a9cb4b88d6d61c81d1', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 'OQezYUwFC2P2JOP81nicQR4qZRB3', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 'C0j9nlTcBaWXmNACgwtnNds0Q3A2', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 'HJtnPGdccnbqsR1V0hWSJe9AWFx1', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', '26ab0db90d72e28ad0ba1e22ee510510', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 'b026324c6904b2a9cb4b88d6d61c81d1', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 'OQezYUwFC2P2JOP81nicQR4qZRB3', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 'C0j9nlTcBaWXmNACgwtnNds0Q3A2', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 'HJtnPGdccnbqsR1V0hWSJe9AWFx1', now(), null);
 
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 1, 'discover');
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 2, 'discover');
@@ -177,16 +226,16 @@ INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('TMnFU6BmQoV8kSMoYYGL
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('TMnFU6BmQoV8kSMoYYGLJDu8qSy1', 1, 'share');
 
 -- Jordan
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', '26ab0db90d72e28ad0ba1e22ee510510', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 'b026324c6904b2a9cb4b88d6d61c81d1', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 'TMnFU6BmQoV8kSMoYYGLJDu8qSy1', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 'C0j9nlTcBaWXmNACgwtnNds0Q3A2', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 'HJtnPGdccnbqsR1V0hWSJe9AWFx1', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', '26ab0db90d72e28ad0ba1e22ee510510', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 'b026324c6904b2a9cb4b88d6d61c81d1', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 'TMnFU6BmQoV8kSMoYYGLJDu8qSy1', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 'C0j9nlTcBaWXmNACgwtnNds0Q3A2', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 'HJtnPGdccnbqsR1V0hWSJe9AWFx1', now(), null);
 
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 2, 'discover');
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 6, 'discover');
@@ -199,16 +248,16 @@ INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('OQezYUwFC2P2JOP81nic
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('OQezYUwFC2P2JOP81nicQR4qZRB3', 2, 'share');
 
 -- Seharsh
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', '26ab0db90d72e28ad0ba1e22ee510510', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 'b026324c6904b2a9cb4b88d6d61c81d1', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 'TMnFU6BmQoV8kSMoYYGLJDu8qSy1', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 'OQezYUwFC2P2JOP81nicQR4qZRB3', now(), null);
-INSERT INTO matches (user_a, user_b, time, rating)
-VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 'HJtnPGdccnbqsR1V0hWSJe9AWFx1', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', '26ab0db90d72e28ad0ba1e22ee510510', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 'b026324c6904b2a9cb4b88d6d61c81d1', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 'TMnFU6BmQoV8kSMoYYGLJDu8qSy1', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 'OQezYUwFC2P2JOP81nicQR4qZRB3', now(), null);
+-- INSERT INTO matches (user_a, user_b, time, rating)
+-- VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 'HJtnPGdccnbqsR1V0hWSJe9AWFx1', now(), null);
 
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 1, 'discover');
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 6, 'discover');
@@ -220,47 +269,17 @@ INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('C0j9nlTcBaWXmNACgwtn
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 6, 'share');
 INSERT INTO user_hobbies (user_id, hobby_id, swap) VALUES ('C0j9nlTcBaWXmNACgwtnNds0Q3A2', 2, 'share');
 
+-- FAKE VOTES
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  'b026324c6904b2a9cb4b88d6d61c81d1', '26ab0db90d72e28ad0ba1e22ee510510');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '26ab0db90d72e28ad0ba1e22ee510510', 'b026324c6904b2a9cb4b88d6d61c81d1');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '6d7fce9fee471194aa8b5b6e47267f03', '48a24b70a0b376535542b996af517398');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  'b026324c6904b2a9cb4b88d6d61c81d1', '48a24b70a0b376535542b996af517398');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), False, 'b026324c6904b2a9cb4b88d6d61c81d1', '6d7fce9fee471194aa8b5b6e47267f03');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '48a24b70a0b376535542b996af517398', 'b026324c6904b2a9cb4b88d6d61c81d1');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '6d7fce9fee471194aa8b5b6e47267f03', '26ab0db90d72e28ad0ba1e22ee510510');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '48a24b70a0b376535542b996af517398', '6d7fce9fee471194aa8b5b6e47267f03');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '6d7fce9fee471194aa8b5b6e47267f03', '48a24b70a0b376535542b996af517398');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '6d7fce9fee471194aa8b5b6e47267f03', 'b026324c6904b2a9cb4b88d6d61c81d1');
 
-
-
-
--- Exluding this for now so we can have testing up for this sprint
-
--- TODO reinstate this in sprint 3
--- CREATE OR REPLACE FUNCTION match()
---     RETURNS TRIGGER
---     LANGUAGE plpgsql
---     AS
--- $make_match$
--- BEGIN
---     IF (
---         SELECT v.user_from
---         FROM votes v
---         WHERE NEW.vote
---           AND v.vote
---           AND v.user_to = new.user_from
---           AND new.user_to = v.user_from
---     ) THEN
---         INSERT INTO matches (user_a, user_b, time, rating)
---           VALUES (NEW.user_from, NEW.user_to, now(), null);
---         DELETE FROM votes WHERE user_to = NEW.user_from;
---         RETURN NULL;
---     END IF;
---     RETURN NEW;
--- END;
--- $make_match$;
-
--- CREATE TRIGGER make_match BEFORE INSERT OR UPDATE ON votes
---     FOR EACH ROW EXECUTE FUNCTION match();
-
-
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  'b026324c6904b2a9cb4b88d6d61c81d1', '26ab0db90d72e28ad0ba1e22ee510510');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '26ab0db90d72e28ad0ba1e22ee510510', 'b026324c6904b2a9cb4b88d6d61c81d1');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '6d7fce9fee471194aa8b5b6e47267f03', '48a24b70a0b376535542b996af517398');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  'b026324c6904b2a9cb4b88d6d61c81d1', '48a24b70a0b376535542b996af517398');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), False, 'b026324c6904b2a9cb4b88d6d61c81d1', '6d7fce9fee471194aa8b5b6e47267f03');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '48a24b70a0b376535542b996af517398', 'b026324c6904b2a9cb4b88d6d61c81d1');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '6d7fce9fee471194aa8b5b6e47267f03', '26ab0db90d72e28ad0ba1e22ee510510');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '48a24b70a0b376535542b996af517398', '6d7fce9fee471194aa8b5b6e47267f03');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '6d7fce9fee471194aa8b5b6e47267f03', '48a24b70a0b376535542b996af517398');
--- INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '6d7fce9fee471194aa8b5b6e47267f03', 'b026324c6904b2a9cb4b88d6d61c81d1');
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  'b026324c6904b2a9cb4b88d6d61c81d1', '26ab0db90d72e28ad0ba1e22ee510510'); -- Repeat match
+INSERT INTO votes (time, vote, user_from, user_to) VALUES (now(), True,  '26ab0db90d72e28ad0ba1e22ee510510', 'b026324c6904b2a9cb4b88d6d61c81d1');
