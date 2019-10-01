2b4beaaab3f17fd3f217ba9c25e504273cf5f4b8
jordanvr <jordash@live.com>
2019-10-01 20:27:09 +0800

    Extra test data

--- server/fadzmaq/api/recs_data.py
+++ server/fadzmaq/api/recs_data.py
@@ -4,7 +4,7 @@ recs = {
             "rank": 1,
             "user": {
                 "user_id": "29f51c08adac957424e06699b81acdb5",
-                "name": "John",
+                "name": "Sam",
                 "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sam_Neill_2010.jpg/435px-Sam_Neill_2010.jpg",
                 "profile_fields": [
                     {
@@ -50,8 +50,8 @@ recs = {
         {
             "rank": 2,
             "user": {
-                "user_id": "29f51c08adac957424e06699b81acdb5",
-                "name": "Amy",
+                "user_id": "29f51c08adac957424e06699b81acdb6",
+                "name": "Jeffery",
                 "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
                 "profile_fields": [
                     {
@@ -96,9 +96,9 @@ recs = {
         },{
             "rank": 2,
             "user": {
-                "user_id": "29f51c08adac957424e06699b81acdb5",
-                "name": "Amy",
-                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+                "user_id": "29f51c08adac957424e06699b81acdb7",
+                "name": "John",
+                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/9/9c/John_Mulaney_at_PaleyFest_2014.jpg",
                 "profile_fields": [
                     {
                         "name": "About me",
@@ -142,9 +142,9 @@ recs = {
         },{
             "rank": 2,
             "user": {
-                "user_id": "29f51c08adac957424e06699b81acdb5",
-                "name": "Amy",
-                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+                "user_id": "29f51c08adac957424e06699b81acdb8",
+                "name": "Aaron",
+                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/e/ed/Aaron_Paul_by_Gage_Skidmore_3.jpg",
                 "profile_fields": [
                     {
                         "name": "About me",
@@ -188,9 +188,9 @@ recs = {
         },{
             "rank": 2,
             "user": {
-                "user_id": "29f51c08adac957424e06699b81acdb5",
-                "name": "Amy",
-                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+                "user_id": "29f51c08adac957424e06699b81acdb9",
+                "name": "Tina",
+                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/4/4e/Tina_Fey_Muppets_Most_Wanted_Premiere_%28cropped%29.jpg",
                 "profile_fields": [
                     {
                         "name": "About me",
@@ -234,9 +234,9 @@ recs = {
         },{
             "rank": 2,
             "user": {
-                "user_id": "29f51c08adac957424e06699b81acdb5",
-                "name": "Amy",
-                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+                "user_id": "29f51c08adac957424e06699b81acd10",
+                "name": "Steve",
+                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/9/96/Steve_Carell_November_2014.jpg",
                 "profile_fields": [
                     {
                         "name": "About me",
@@ -280,9 +280,9 @@ recs = {
         },{
             "rank": 2,
             "user": {
-                "user_id": "29f51c08adac957424e06699b81acdb5",
-                "name": "Amy",
-                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+                "user_id": "29f51c08adac957424e06699b81acd11",
+                "name": "Jim",
+                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/8/8b/Jim_Carrey_2008.jpg",
                 "profile_fields": [
                     {
                         "name": "About me",
@@ -326,9 +326,9 @@ recs = {
         },{
             "rank": 2,
             "user": {
-                "user_id": "29f51c08adac957424e06699b81acdb5",
-                "name": "Amy",
-                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg/800px-Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+                "user_id": "29f51c08adac957424e06699b81acd12",
+                "name": "Stanley",
+                "photo_location": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Stanley_Tucci_2017_Berlinale.jpg/800px-Stanley_Tucci_2017_Berlinale.jpg",
                 "profile_fields": [
                     {
                         "name": "About me",
