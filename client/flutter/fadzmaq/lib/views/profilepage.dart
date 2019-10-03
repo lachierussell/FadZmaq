804e494f7e42458567470f02f508a6053d2ac12f
jordanvr <jordash@live.com>
2019-10-03 12:04:57 +0800

    Reformat of profile page

--- client/flutter/fadzmaq/lib/views/profilepage.dart
+++ client/flutter/fadzmaq/lib/views/profilepage.dart
@@ -29,9 +29,9 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Profile'),
-      ),
+      // appBar: AppBar(
+      // title: Text('My Profile'),
+      // ),
       body: GetRequest<ProfileData>(
         url: url,
         builder: (context) {
@@ -53,7 +53,6 @@ class ProfilePic extends StatelessWidget {
       fit: BoxFit.cover,
       // placeholder: (context, url) => new CircularProgressIndicator(),
       errorWidget: (context, url, error) => new Icon(Icons.error),
-      
     );
   }
 }
@@ -69,43 +68,30 @@ class ProfilePageState extends StatelessWidget {
 
     // putting these up here in case of nulls
     // right now just putting dash instead of the value
-    final String profileAge = pd.age != null ? pd.age : "-";
+    // final String profileAge = pd.age != null ? pd.age : "-";
     final String profileName = pd.age != null ? pd.name : "-";
 
-    String hobbiesRaw = "";
-
-    print(pd.hobbyContainers.toString());
-
-    if (pd.hobbyContainers != null) {
-      for (HobbyContainer hc in pd.hobbyContainers) {
-        hobbiesRaw += "--" + hc.container + "--\n";
-        if (hc.hobbies != null) {
-          for (HobbyData h in hc.hobbies) {
-            hobbiesRaw += h.name + "\n";
-          }
-        }
-      }
-    }
 
     // final String image = 'assets/images/glenn.jpg';
     return Scaffold(
       body: SingleChildScrollView(
         child: Stack(
           children: <Widget>[
-            Container(
-              height: 240,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(50.0),
-                      bottomRight: Radius.circular(50.0)),
-                  gradient: LinearGradient(
-                      colors: [color1, color2],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight)),
-            ),
+            // Container(
+            //   height: 240,
+            //   decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.only(
+            //           bottomLeft: Radius.circular(50.0),
+            //           bottomRight: Radius.circular(50.0)),
+            //       gradient: LinearGradient(
+            //           colors: [color1, color2],
+            //           begin: Alignment.topLeft,
+            //           end: Alignment.bottomRight)),
+            // ),
             Container(
               // margin: const EdgeInsets.only(top: 80),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   // SizedBox(height: 20.0),
                   // Expanded(
@@ -113,79 +99,65 @@ class ProfilePageState extends StatelessWidget {
                   // children: <Widget>[
                   Container(
                     // height: double.infinity,
-                    margin: const EdgeInsets.only(
-                        left: 40.0, right: 40.0, top: 5.0, bottom: 0.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30.0),
-                      // child: Image.asset(image,fit: BoxFit.cover,),
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: ProfilePic(),
-                      ),
+                    // margin: const EdgeInsets.only(
+                    // left: 40.0, right: 40.0, top: 5.0, bottom: 0.0),
+                    // child: ClipRRect(
+                    // borderRadius: BorderRadius.circular(30.0),
+                    // child: Image.asset(image,fit: BoxFit.cover,),
+                    child: AspectRatio(
+                      aspectRatio: 0.75,
+                      child: ProfilePic(),
                     ),
+                    // ),
                   ),
-
-                  SizedBox(height: 15.0),
-                  Text(
-                    profileName + ', ' + profileAge,
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 34.0,
-                        height: 1.5
-                    ),
-                  ),
-                  Align
-                    (
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 40.0, right: 40.0, top: 5.0),
-                      child: Text("Discovering"),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 40.0, right: 40.0, top: 5.0),
-                    child: Align
-                      (
-                      alignment: Alignment.centerLeft,
-                      child: Container(
-                          child: Wrap(
-                            spacing: 5.0,
-                            runSpacing: 3.0,
-                            children: <Widget>[
-                              getHobbies(context, pd, "discover")
-                            ],
-                          )
-                      ),
-                    ),
-                  ),
-                  Align
-                    (
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 40.0, right: 40.0, top: 15.0),
-                      child: Text("Sharing"),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 40.0, right: 40.0, top: 5.0),
-                    child: Align
-                      (
-                      alignment: Alignment.centerLeft,
-                      child: Container(
-                          child: Wrap(
-                            spacing: 5.0,
-                            runSpacing: 3.0,
-                            children: <Widget>[
-                              getHobbies(context, pd, "share")
-                            ],
-                          )
-                      ),
-                    ),
-                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 16, right: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        // SizedBox(height: 15.0),
+                        Text(
+                          profileName,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 42.0,
+                              height: 1.5),
+                        ),
+                        Divider(
+                          color: Colors.grey,
+                        ),
+                        Text("Discovering",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Container(
+                            child: Wrap(
+                          spacing: 5.0,
+                          runSpacing: 3.0,
+                          children: <Widget>[
+                            getHobbies(context, pd, "discover")
+                          ],
+                        )),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text("Sharing",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Container(
+                            child: Wrap(
+                          spacing: 5.0,
+                          runSpacing: 3.0,
+                          children: <Widget>[getHobbies(context, pd, "share")],
+                        )),
+                        Divider(
+                          color: Colors.grey,
+                        ),
+                                                Text(
+                          "5km away",
+                          // style: TextStyle(
+                          //     fontWeight: FontWeight.bold,
+                          //     fontSize: 42.0,
+                          //     height: 1.5),
+                        ),
+                        SizedBox(height: 15,)
 //                  Text(
 //                    hobbiesRaw,
 //                    style: TextStyle(
@@ -193,79 +165,81 @@ class ProfilePageState extends StatelessWidget {
 //                      height: 2.0,
 //                    ),
 //                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 50.0),
-                      ),
-                      Icon(
-                        Icons.location_on,
-                        size: 16.0,
-                        color: Colors.grey,
-                      ),
-                      Text(
-                        "Perth, Western Australia, Australia",
-                        style: TextStyle(color: Colors.grey.shade600),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 5.0),
-                  
-                  SizedBox(height: 10.0),
-                  Container(
-                    child: Stack(
-                      children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 5.0, horizontal: 16.0),
-                          margin: const EdgeInsets.only(
-                              top: 30, left: 20.0, right: 20.0, bottom: 20.0),
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [color1, color2],
-                              ),
-                              borderRadius: BorderRadius.circular(30.0)),
-                          child: Row(
-                            children: <Widget>[
-                              IconButton(
-                                  color: Colors.white,
-                                  icon: Icon(CupertinoIcons.person_solid),
-                                  onPressed: () {}),
-                              IconButton(
-                                color: Colors.white,
-                                icon: Icon(Icons.location_on),
-                                onPressed: () {},
-                              ),
-                              Spacer(),
-                              IconButton(
-                                color: Colors.white,
-                                icon: Icon(Icons.add),
-                                onPressed: () {},
-                              ),
-                              IconButton(
-                                color: Colors.white,
-                                icon: Icon(Icons.message),
-                                onPressed: () {},
-                              ),
-                            ],
-                          ),
-                        ),
-                        Center(
-                          child: FloatingActionButton(
-                            child: Icon(
-                              Icons.favorite,
-                              color: Colors.pink,
-                            ),
-                            backgroundColor: Colors.white,
-                            onPressed: () {},
-                          ),
-                        ),
                       ],
                     ),
-                  )
+                  ),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   crossAxisAlignment: CrossAxisAlignment.center,
+                  //   children: <Widget>[
+                  //     Padding(
+                  //       padding: const EdgeInsets.only(top: 50.0),
+                  //     ),
+                  //     Icon(
+                  //       Icons.location_on,
+                  //       size: 16.0,
+                  //       color: Colors.grey,
+                  //     ),
+                  //     Text(
+                  //       "Perth, Western Australia, Australia",
+                  //       style: TextStyle(color: Colors.grey.shade600),
+                  //     )
+                  //   ],
+                  // ),
+                  // SizedBox(height: 5.0),
+
+                  // SizedBox(height: 10.0),
+                  // Container(
+                  //   child: Stack(
+                  //     children: <Widget>[
+                  //       Container(
+                  //         padding: const EdgeInsets.symmetric(
+                  //             vertical: 5.0, horizontal: 16.0),
+                  //         margin: const EdgeInsets.only(
+                  //             top: 30, left: 20.0, right: 20.0, bottom: 20.0),
+                  //         decoration: BoxDecoration(
+                  //             gradient: LinearGradient(
+                  //               colors: [color1, color2],
+                  //             ),
+                  //             borderRadius: BorderRadius.circular(30.0)),
+                  //         child: Row(
+                  //           children: <Widget>[
+                  //             IconButton(
+                  //                 color: Colors.white,
+                  //                 icon: Icon(CupertinoIcons.person_solid),
+                  //                 onPressed: () {}),
+                  //             IconButton(
+                  //               color: Colors.white,
+                  //               icon: Icon(Icons.location_on),
+                  //               onPressed: () {},
+                  //             ),
+                  //             Spacer(),
+                  //             IconButton(
+                  //               color: Colors.white,
+                  //               icon: Icon(Icons.add),
+                  //               onPressed: () {},
+                  //             ),
+                  //             IconButton(
+                  //               color: Colors.white,
+                  //               icon: Icon(Icons.message),
+                  //               onPressed: () {},
+                  //             ),
+                  //           ],
+                  //         ),
+                  //       ),
+                  //       Center(
+                  //         child: FloatingActionButton(
+                  //           child: Icon(
+                  //             Icons.favorite,
+                  //             color: Colors.pink,
+                  //           ),
+                  //           backgroundColor: Colors.white,
+                  //           onPressed: () {},
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // )
                 ],
               ),
             ),
@@ -276,28 +250,24 @@ class ProfilePageState extends StatelessWidget {
   }
 }
 
-class FilterChipWidget extends StatefulWidget {
-  final String chipName;
-  String hobbyContainer;
-  FilterChipWidget({Key key, this.chipName}) : super(key : key);
+// class FilterChipWidget extends StatefulWidget {
+//   final String chipName;
+//   String hobbyContainer;
+//   FilterChipWidget({Key key, this.chipName}) : super(key: key);
 
-  @override
-  _FilterChipWidgetState createState() => _FilterChipWidgetState();
+//   @override
+//   _FilterChipWidgetState createState() => _FilterChipWidgetState();
+// }
 
-}
-
-class _FilterChipWidgetState extends State<FilterChipWidget>{
-
-  Widget build(BuildContext context){
-    return FilterChip(
-      label:Text(widget.chipName),
-      labelStyle: TextStyle(color: Color(0xff6200ee),fontSize: 16.0),
-      onSelected: (b){},
-
-    );
-  }
-
-}
+// class _FilterChipWidgetState extends State<FilterChipWidget> {
+//   Widget build(BuildContext context) {
+//     return FilterChip(
+//       label: Text(widget.chipName),
+//       labelStyle: TextStyle(color: Color(0xff6200ee), fontSize: 16.0),
+//       onSelected: (b) {},
+//     );
+//   }
+// }
 
 Widget getHobbies(BuildContext context, ProfileData profile, String container) {
   // return Text("to be done");
@@ -334,10 +304,10 @@ Widget getHobbyChip(BuildContext context, HobbyData hobby) {
     child: Container(
       color: Color(0xfff2f2f2),
       child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 14, 20, 14),
+          // padding: const EdgeInsets.fromLTRB(20, 14, 20, 14),
+          padding: const EdgeInsets.fromLTRB(10, 4, 10, 4),
           // child: new Text(hobby.name, style: Theme.of(context).textTheme.body1),
           child: new Text(hobby.name)),
     ),
   );
 }
-
