ea9bde49f7bdd2d05a230d436237539e510e05b8
jordanvr <jordash@live.com>
2019-10-03 13:12:58 +0800

    Start of smart hobby chips

--- client/flutter/fadzmaq/lib/views/widgets/hobbyChip.dart
+++ client/flutter/fadzmaq/lib/views/widgets/hobbyChip.dart
@@ -14,6 +14,89 @@ class HobbyChips extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // get list to show
+    // get list to compare
+
+    // for each in show
+    // for each in compare
+    // if equal record
+
+    // do twice
+
+    // for each in one
+    // for each in two
+    // if the same record for both
+
+    // one
+    // return one
+
+    // two
+    // return two
+
+    // matches
+    // for each in one add
+    // for each in two check if already in one
+    // if in change one
+    // else add two
+
+    List<HobbyInfo> listMyShare = new List<HobbyInfo>();
+    List<HobbyInfo> listMyDiscover = new List<HobbyInfo>();
+    List<HobbyInfo> listShare = new List<HobbyInfo>();
+    List<HobbyInfo> listDiscover = new List<HobbyInfo>();
+
+    if (hobbies != null) {
+      for (HobbyContainer hc in hobbies) {
+        // print(hc.container.toString());
+        if (hc.container == "share") {
+          for (HobbyData hobby in hc.hobbies) {
+            listShare.add(HobbyInfo(hobby: hobby));
+          }
+        }
+        if (hc.container == "discover") {
+          for (HobbyData hobby in hc.hobbies) {
+            listDiscover.add(HobbyInfo(hobby: hobby));
+          }
+        }
+        if (hc.container == "my_share") {
+          for (HobbyData hobby in hc.hobbies) {
+            listMyShare.add(HobbyInfo(hobby: hobby));
+          }
+        }
+        if (hc.container == "my_discover") {
+          for (HobbyData hobby in hc.hobbies) {
+            listMyDiscover.add(HobbyInfo(hobby: hobby));
+          }
+        }
+      }
+    }
+
+    for (HobbyInfo share in listShare) {
+      for (HobbyInfo mine in listMyDiscover) {
+        if (share.hobby == mine.hobby) {
+          share.direction = HobbyDirection.discover;
+        }
+      }
+    }
+
+    for (HobbyInfo discover in listDiscover) {
+      for (HobbyInfo mine in listMyShare) {
+        if (discover.hobby == mine.hobby) {
+          discover.direction = HobbyDirection.share;
+        }
+      }
+    }
+
+    for (HobbyInfo discover in listDiscover) {
+      for (HobbyInfo share in listShare) {
+        if (discover.hobby == share.hobby &&
+            discover.direction != HobbyDirection.none &&
+            share.direction != HobbyDirection.none) {
+              discover.direction = HobbyDirection.match;
+              share.direction = HobbyDirection.match;
+            }
+      }
+    }
+
     List<Widget> list = new List<Widget>();
     // print(profile.hobbyContainers.toString());
     if (hobbies != null) {
@@ -35,6 +118,23 @@ class HobbyChips extends StatelessWidget {
       ),
     );
   }
+}
+
+enum HobbyDirection {
+  none,
+  match,
+  share,
+  discover,
+}
+
+class HobbyInfo {
+  final HobbyData hobby;
+  HobbyDirection direction;
+
+  HobbyInfo({
+    this.hobby,
+    this.direction = HobbyDirection.none,
+  });
 }
 
 class HobbyChip extends StatelessWidget {
