a1abb99f2e40a98946d259cbe9fd702f0495a5c8
jordanvr <jordash@live.com>
2019-10-03 23:40:30 +0800

    Hobby Chip overhaul

--- client/flutter/fadzmaq/lib/views/widgets/hobbyChip.dart
+++ client/flutter/fadzmaq/lib/views/widgets/hobbyChip.dart
@@ -1,6 +1,7 @@
 import 'package:fadzmaq/models/hobbies.dart';
 import 'package:fadzmaq/models/profile.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class HobbyChips extends StatelessWidget {
   final List<HobbyContainer> hobbies;
@@ -172,33 +173,190 @@ class HobbyChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // return Chip(
+    //   label: Text(hobby.hobby.name, style: _hobbyStyle),
+    //   backgroundColor: getColor(hobby.direction),
+    //   // padding: EdgeInsets.all(0),
+    //   labelPadding: EdgeInsets.all(0),
+    //   avatar: CircleAvatar(
+    //     child: Icon( Icons.ac_unit),
+    //   ),
+
+    // );
+
     return ClipRRect(
       borderRadius: BorderRadius.all(Radius.circular(32)),
-      child: Container(
-        color: getColor(hobby.direction),
-        child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 4, 10, 4),
-            // child: new Text(hobby.name, style: Theme.of(context).textTheme.body1),
-            child: new Text(hobby.hobby.name, style: _hobbyStyle)),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            color: getColor(hobby.direction, -0.17),
+            height: 30,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(3, 4, 10, 4),
+              // child: new Text(hobby.name, style: Theme.of(context).textTheme.body1),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  HobbyIconBackground(direction: hobby.direction),
+                  Text(hobby.hobby.name, style: _hobbyStyle),
+                ],
+              ),
+            ),
+          ),
+          HobbyDirectionIcon(direction: hobby.direction),
+        ],
       ),
     );
   }
 }
 
-Color getColor(HobbyDirection direction) {
-  switch (direction) {
-    case HobbyDirection.discover:
-      return Colors.red;
-    case HobbyDirection.share:
-      return Colors.blue;
-    case HobbyDirection.match:
-      return Colors.purple;
-    default:
-      return Color(0xffd9d9d9);
+class HobbyIconBackground extends StatelessWidget {
+  const HobbyIconBackground({
+    this.direction,
+  });
+
+  final HobbyDirection direction;
+
+  @override
+  Widget build(BuildContext context) {
+    if (direction == null || direction == HobbyDirection.none) {
+      return SizedBox(
+        width: 7,
+      );
+    }
+
+    return Row(
+      children: <Widget>[
+        Container(
+          height: 24,
+          width: 24,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: getColor(direction, 0.1),
+          ),
+        ),
+        SizedBox(
+          width: 4,
+        ),
+      ],
+    );
   }
 }
+
+class HobbyDirectionIcon extends StatelessWidget {
+  const HobbyDirectionIcon({
+    Key key,
+    @required this.direction,
+  }) : super(key: key);
+
+  final HobbyDirection direction;
+
+  @override
+  Widget build(BuildContext context) {
+    const double height = 30;
+    const double size = 26;
+    const shade = -0.2;
+
+    final Widget share = Container(
+      height: height,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0.5, 1, 0, 0),
+        child: Icon(
+          Icons.keyboard_arrow_left,
+          color: getColor(direction, shade),
+          size: size,
+        ),
+      ),
+    );
+
+    final Widget discover = Container(
+      height: height,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(2.5, 1, 0, 0),
+        child: Icon(
+          Icons.keyboard_arrow_right,
+          color: getColor(direction, shade),
+          size: size,
+        ),
+      ),
+    );
+
+    final Widget match = Container(
+      height: height,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8.8, 5.4, 0, 0),
+            child: Icon(
+              Icons.keyboard_arrow_right,
+              color: getColor(direction, shade),
+              size: 20,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(1, 5.8, 0, 0),
+            child: Icon(
+              Icons.keyboard_arrow_left,
+              color: getColor(direction, shade),
+              size: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    switch (direction) {
+      case HobbyDirection.discover:
+        return discover;
+      case HobbyDirection.match:
+        return match;
+      case HobbyDirection.share:
+        return share;
+      default:
+        return Container(height: height, width: 0,);
+    }
+  }
+}
+
+Color getColor(HobbyDirection direction, double shade) {
+  switch (direction) {
+    case HobbyDirection.discover:
+      return adjustShade(discoverC, shade);
+    case HobbyDirection.share:
+      return adjustShade(shareC, shade);
+    case HobbyDirection.match:
+      return adjustShade(matchC, shade);
+    default:
+      return Color(0xffb5b5b5);
+  }
+}
+
+Color adjustShade(Color col, double shade) {
+  HSLColor hsl = HSLColor.fromColor(col);
+
+  double lightness = hsl.lightness + shade;
+  lightness = max(0, lightness);
+  lightness = min(1, lightness);
+
+  HSLColor result = HSLColor.fromAHSL(1, hsl.hue, hsl.saturation, lightness);
+
+  return result.toColor();
+}
+
+Color matchC = Color(0xffB980FF);
+Color discoverC = Color(0xffeb769f);
+Color shareC = Color(0xff80B7FF);
+
+// Color purple = Color(0xff947bff);
+// Color red = Color(0xffff5785);
+// Color blue = Color(0xff73aeff);
+
+// Color purpleD = Color(0xffa861ff);
+// Color redD = Color(0xffee2961);
+// Color blueD = Color(0xff4fa2ff);
 
 final TextStyle _hobbyStyle = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.w400,
+  color: Colors.white,
 );
