e20f83d05f71722bc7d6fba723b888aeff66ba54
jordanvr <jordash@live.com>
2019-10-08 00:30:28 +0800

    Random account initial

---
+++ server/tests/random_account_data.py
@@ -0,0 +1,55 @@
+male_name_list = [
+    "Sam",
+    "John",
+]
+
+female_name_list = [
+    "Tina",
+    "Joan",
+]
+
+female_photo_list = [
+    "https://upload.wikimedia.org/wikipedia/commons/1/10/Rooney_Mara_at_The_Discovery_premiere_during_day_2_of_the_2017_Sundance_Film_Festival_at_Eccles_Center_Theatre_on_January_20%2C_2017_in_Park_City%2C_Utah_%2832088061480%29_%28cropped%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/4/4e/Tina_Fey_Muppets_Most_Wanted_Premiere_%28cropped%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/a/a4/Kate_Mara_by_Gage_Skidmore.jpg/220px-Kate_Mara_by_Gage_Skidmore.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/a/ac/Marisa_Tomei_at_Toronto_Film_Festival_2012.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/0/01/Michelle_Williams_%28Berlinale_2010%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Amy_Adams_%28actress%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/2/25/Laura_Linney_Berlinale_2017.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/0/03/Julianne_Moore_Cannes_2018_%28tweaked%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/4/46/Meryl_Streep_December_2018.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/3/37/Cate_Blanchett_Cannes_2018_2_%28cropped%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/4/47/Diane_Sawyer_2011_Shankbone.JPG",
+    "https://upload.wikimedia.org/wikipedia/commons/b/bc/Laura_Dern_Deauville_2017.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/a/a9/Kate_Mara_2008.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/7/78/Jessica_Chastain_at_2015_Empire_Awards.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/5/55/Kristen_Wiig_TIFF_2014.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/0/0d/%27The_Martian%27_World_Premiere_%28NHQ201509110005%29_cropped.jpg",
+
+]
+
+male_photo_list = [
+    "https://upload.wikimedia.org/wikipedia/commons/6/61/Sam_Neill_2010.jpg/435px-Sam_Neill_2010.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/5/51/Jeffrey_Wright_by_Gage_Skidmore_3.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/4/41/Joaquin_Phoenix_at_the_2018_Berlin_Film_Festival.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/3/31/Joaquin_Cannes_20002_cropped.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/c/c3/Joaquin_Phoenix_Cannes_2017.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/5/58/Robert_De_Niro_Cannes_2016.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/5/58/Robert_De_Niro_Cannes_2016.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/f/f5/Philip_Seymour_Hoffman_2011.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/e/eb/Paul_Thomas_Anderson_2007_crop.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/d/d5/Ethan_Hawke_2018.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/5/57/Chris_Rock_WE_2012_Shankbone.JPG",
+    "https://upload.wikimedia.org/wikipedia/commons/4/4a/Sam_Rockwell_%288279227257%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/5/53/Rami_Malek.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/d/db/Jesse_Plemons_%2820769593584%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/0/00/Jamie_Bell-8448_%28cropped%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/8/83/Matt_Damon_TIFF_2015.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/7/75/Jeff_Daniels_May_2018_%28cropped%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/b/b8/Chiwetel_Ejiofor_by_Gage_Skidmore.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/c/ca/AV0A6306_Sean_Bean.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/1/14/Michael_Pe%C3%B1a_TIFF_2015.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/e/ee/Sebastian_Stan_by_Gage_Skidmore_2_%28cropped%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/2/2a/Donald_Glover_TIFF_2015.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/7/7c/Benedict_Wong%2C_2016_%28cropped%29.jpg",
+]
