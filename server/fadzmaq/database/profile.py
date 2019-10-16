628c85f400c875319e04e3b93e1dc3ed944ccbd9
Lachlan Russell <lachie.russell@gmail.com>
2019-10-16 09:25:49 +0800

    profile docs

--- server/fadzmaq/database/profile.py
+++ server/fadzmaq/database/profile.py
@@ -1,4 +1,5 @@
 # @file
+# @brief Builds profile data and retrieves profile information.
 #
 # FadZmaq Project
 # Professional Computing. Semester 2 2019
@@ -6,14 +7,22 @@
 # Copyright FadZmaq © 2019      All rights reserved.
 # @author Lachlan Russell       22414249@student.uwa.edu.au
 # @author Jordan Russell        20357813@student.uwa.edu.au
-
+# @author Thiren Naidoo         22257963@student.uwa.edu.au
+# @author Beining Chen          22384298@student.uwa.edu.au
 
 import fadzmaq.database.connection as db
 from fadzmaq.database.hobbies import get_hobbies
 
 
+## @brief Updates a users profile.
+# @param subject    The posted values
+# @param uid        Your user id.
+# @warning For some reason this function uses HTML form data rather than
+# JSON like the rest of the project. This was marked to be fixed by the team has been too
+# pushed for time implementing all the other features.
+# @warning Currently no input validation.
 def update_profile(subject, uid):
-    rows = db.get_db().execute(
+    db.get_db().execute(
         '''
         UPDATE profile
         SET nickname= %s, bio= %s, email= %s, phone= %s, photo= %s
@@ -23,6 +32,14 @@ def update_profile(subject, uid):
     )
 
 
+## @brief Builds a profiles user data into an object.
+# This is a dynamic build depending on the permissions of the caller.
+# e.g. a recommendation profile will only contain age and bio, but a match will also contain contact information.
+# @param row        A ResultProxy row to extract values from.
+# @param permission The permission value of the caller. 0 is recs, 1 is matches, 2 is account
+# @return A profile object.
+# @note This is not JSON, convert to JSON with `jsonify()`.
+# @warning Location is currently excluded from the data.
 def build_profile_data(row, permission):
     assert type(permission) is int
     assert permission <= 2
@@ -30,7 +47,6 @@ def build_profile_data(row, permission):
     assert row is not None, "Query retrieved no rows to build profile."
 
     profile_fields = []
-    # permission_keys = [['bio', 'age', 'location'],
     permission_keys = [['bio', 'location'],
                        ['phone', 'email'],
                        ['birth-date']
@@ -59,9 +75,10 @@ def build_profile_data(row, permission):
     return profile
 
 
-# Retrieves profile information for the subject.
-# @param    subject     user_id for the database entry
-# @return   json profile data or raises value error.
+## @brief Retrieves profile information for the subject.
+# @param    subject     user_id for the profile.
+# @return   Profile data object.
+# @note This is not JSON, convert to JSON with `jsonify()`.
 def retrieve_profile(subject):
     # Retrieves user info.
     rows = db.get_db().execute(
@@ -71,11 +88,11 @@ def retrieve_profile(subject):
         WHERE user_id = %s
         ''', subject
     )
-
     return build_profile_data(rows.first(), 2)
 
 
-# @brief Verifies the user is in the database
+## @brief Verifies the user is in the database.
+# @param subject    The user id to check if exists.
 # @returns True if the user exists
 def verify_user(subject):
     rows = db.get_db().execute(
@@ -92,8 +109,12 @@ def verify_user(subject):
     return False
 
 
-# @brief Creates a user in the database
+## @brief Creates a user in the database
+# @param name   The users nickname.
+# @param email  The users email.
+# @param uid    The users ID.
 # @throws IOError if the user already exists or the database insertion fails.
+# @warning Currently no input validation.
 def make_user(name, email, uid):
     rows = db.get_db().execute(
         '''
@@ -107,9 +128,11 @@ def make_user(name, email, uid):
     raise IOError
 
 
-# @brief Performs a cascade delete on all the user information
-# This will revoke and previous matches, likes, ratings and locations
+## @brief Performs a cascade delete on all the user information
+# This will revoke any previous matches, likes, ratings and locations
 # associated with this user.
+# @param uid    The user ID to delete from the database.
+# @warning This function is **NOT** reversible.
 def delete_account(uid):
     db.get_db().execute(
         '''
@@ -118,6 +141,10 @@ def delete_account(uid):
     )
 
 
+## @brief Retrieves the distance setting from the database.
+# @param uid    The users id.
+# @return A object containing the distance setting.
+# @note This is not JSON, convert to JSON with `jsonify()`.
 def retrieve_settings(uid):
     rows = db.get_db().execute(
         '''
@@ -131,6 +158,10 @@ def retrieve_settings(uid):
     }
 
 
+## @brief Updates the distance settings for a particular user
+# @param uid        The users id.
+# @param value      The new distance value.
+# @warning Currently no input validation.
 def update_settings(uid, value):
     db.get_db().execute(
         '''
@@ -141,6 +172,13 @@ def update_settings(uid, value):
     )
 
 
+## @brief Sets the user location and device id.
+# This records the user location. It should be reduced to 2.S.F on the device to reduce the risk of sensitive
+# information being leaked.
+# @param uid    The user id.
+# @param lat    The latitude.
+# @param long   The longitude.
+# @param dev    The FCM device registration token.
 def set_location(uid, lat, long, dev):
     db.get_db().execute(
         '''
