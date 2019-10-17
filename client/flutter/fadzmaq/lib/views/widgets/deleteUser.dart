43dc5996bc0744abc314b95e09b8b4e5edac4756
jordanvr <jordash@live.com>
2019-10-17 16:20:07 +0800

    formatting

--- client/flutter/fadzmaq/lib/views/widgets/deleteUser.dart
+++ client/flutter/fadzmaq/lib/views/widgets/deleteUser.dart
@@ -5,20 +5,20 @@ import 'package:flutter/cupertino.dart';
 /// Alert diaglog to confirm deleting an account
 void deleteDialog(BuildContext context) async {
   showDialog(
-  context: context,
-  barrierDismissible: false, // user must tap button for close dialog!
-  builder: (BuildContext context) {
-  return AlertDialog(
-  title: Text('Are you sure?'),
-  content: const Text(
-  'Please be aware that this action will delete your data too, but you can still log in with this account next time.'),
-  actions: <Widget>[
-  FlatButton(
-  child: const Text('CANCEL'),
-  onPressed: () {
-  Navigator.of(context).pop();
-  },
-  ),
+    context: context,
+    barrierDismissible: false, // user must tap button for close dialog!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Are you sure?'),
+        content: const Text(
+            'Please be aware that this action will delete your data too, but you can still log in with this account next time.'),
+        actions: <Widget>[
+          FlatButton(
+            child: const Text('CANCEL'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
           FlatButton(
               child: const Text('ACCEPT'),
               onPressed: () {
